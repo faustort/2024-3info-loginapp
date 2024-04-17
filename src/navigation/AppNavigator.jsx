03e5e44b524dc4ca4bd12d659aec678cbea9b800
Your Name <you@example.com>
2024-04-17 14:54:43 -0300

    Add RegisterScreen to AppNavigator and implement login functionality in LoginScreen

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
 import HomeScreen from "../screens/HomeScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,17 +11,24 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            title: "Login",
+          }}
+        />
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
           options={{
             title: "Inicial",
           }}
         />
         <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
+          name="HomeScreen"
+          component={HomeScreen}
           options={{
-            title: "Login",
+            title: "Inicial",
           }}
         />
       </Stack.Navigator>
